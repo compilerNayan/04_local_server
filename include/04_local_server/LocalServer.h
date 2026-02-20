e5a2a640cfa566f9abdd46495c8ac1cd617edc97
Nayan Kurude <compilerNayan@gmail.com>
2026-02-20 15:24:05 +0530

    kdjjkads

---
+++ include/04_local_server/LocalServer.h
@@ -0,0 +1,12 @@
+/**
+ * @file LocalServer.h
+ * @brief Umbrella header for 04_local_server (springbootplusplus_web).
+ * Include this or use springbootplusplus_web headers directly (HttpRequestManager.h, etc.).
+ */
+
+#ifndef LOCAL_SERVER_LOCAL_SERVER_H
+#define LOCAL_SERVER_LOCAL_SERVER_H
+
+#include <HttpRequestManager.h>
+
+#endif  // LOCAL_SERVER_LOCAL_SERVER_H
